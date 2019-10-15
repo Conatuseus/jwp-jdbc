02ff3479e0d90e5434d65e312ab8a73657e073d1
MK S <conatuseus@gmail.com>
2019-10-15 13:35:16 +0900

    feat: 유저 findAll 기능 추가

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -36,7 +36,6 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
@@ -60,8 +59,37 @@ public class UserDao {
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        List<User> users = new ArrayList<>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT * FROM USERS";
+            pstmt = con.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                User user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                    rs.getString("email"));
+
+                users.add(user);
+            }
+            return users;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     public User findByUserId(String userId) throws SQLException {
@@ -79,7 +107,7 @@ public class UserDao {
             User user = null;
             if (rs.next()) {
                 user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
+                    rs.getString("email"));
             }
 
             return user;
